6e168034c5d0addab454db1d1aeec91fc5fee907
root <root@localhost.localdomain>
2020-07-29 11:34:23 +0530

    create basic template

---
+++ chat/consumers.py
@@ -0,0 +1,11 @@
+import asyncio
+import json
+from django.contrib.auth import get_user_model
+from channels.consumer import AsyncConsumer
+from channels.db import database_sync_to_async
+
+from .models import Thread, ChatMessage
+
+class ChatConsumet(AsyncConsumer):
+    def websocket_connect(self, event):
+        print("connected",event)
