605848458e70f08166265761f178dbf3b27843c4
root <root@localhost.localdomain>
2020-07-29 13:57:47 +0530

    Accept Websocket connection

--- chat/consumers.py
+++ chat/consumers.py
@@ -7,5 +7,19 @@ from channels.db import database_sync_to_async
 from .models import Thread, ChatMessage
 
 class ChatConsumer(AsyncConsumer):
-    def websocket_connect(self, event):
+    async def websocket_connect(self, event):
         print("connected",event)
+        await self.send({
+            "type": "websocket.accept"
+        })
+        # await asyncio.sleep(10)
+        await self.send({
+            "type": "websocket.send",
+            "text": "Hello"
+        })
+
+    async def websocket_receive(self, event):
+        print("received",event)
+
+    async def websocket_disconnect(self, event):
+        print("disconnected",event)
